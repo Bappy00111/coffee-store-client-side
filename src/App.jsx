0613e8dc87a7826ba94f738153635aafdb09f778
Bappy Modok <121638025+Bappy00111@users.noreply.github.com>
2024-05-26 09:49:06 +0600

    get the added all coffee

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const App = () => {
+  const data = useLoaderData();
+  console.log(data)
   return (
     <div>
-      <h1>hellow</h1>
+      <h1>Add Coffe :</h1>
+      {
+        data.map(d =><p key={d._id}>{d.name}</p>)
+      }
     </div>
   );
 };
